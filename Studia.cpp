7416d6f2ad5fa757c4fc85b387d924cb4160de01
Łukasz Lampart <lukaszlampart@localhost.localdomain>
2015-05-17 13:47:41 +0200

    Studia - drugie podejście

--- Studia.cpp
+++ Studia.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cstdio>
 #include <string>
+#include <algorithm>
 class Solver {
 private:
 	/**
@@ -22,26 +23,37 @@ private:
 	};
 	std::vector<Edge> edges;
 	std::vector<long long> d;
+	std::vector<std::vector<int>> tGraph;
+	std::vector<bool> visited;
 	int V;
 public:
 	struct Solution {
 		int profitNumb;
 		std::vector<int> nodes;
+		void operator+(const int& node) {
+			this->profitNumb++;
+			this->nodes.push_back(node);
+		}
 	};
 private:
 	Solution solution;
+	void addNodes(int v);
 public:
 	inline Solver(size_t n,size_t m,size_t s): V(n){
 		d.resize(n+1);
+		tGraph.resize(n+1);
+		visited.resize(n+1);
 		long long plusInf=1<<31;
 		for (int i=1; i<=n; i++){
 			d[i]=plusInf;
+			visited[i]=false;
 		}
 		d[s]=0;
 	}
 	inline void pushEdge(int u,int v,int weight){
 		Edge e(u,v,weight);
 		edges.push_back(e);
+		tGraph[v].push_back(u); // tworzymy graf transponowany, potrzebny do stworzenia wyniku
 	}
 	Solution& solve();
 };
@@ -56,6 +68,7 @@ int main(){
 		s.pushEdge(a,b,c);
 	}
 	Solver::Solution& solution=s.solve();
+	std::sort(solution.nodes.begin(),solution.nodes.end());
 	printf("%d\n",solution.profitNumb);
 	for (int node:solution.nodes){
 		printf("%d ",node);
@@ -64,7 +77,19 @@ int main(){
 	return 0;
 }
 
+void Solver::addNodes(int v) {
+	for (int u: tGraph[v]) {
+		if (!visited[u]) {
+			solution+u;
+			visited[u]=true;
+			addNodes(u);
+		}
+	}
+}
+
 Solver::Solution& Solver::solve() {
+	solution.nodes.clear();
+	solution.profitNumb=0;
 	bool wasChanged=true;
 	for (int i=1; i<V && wasChanged; i++){
 		wasChanged=false;
@@ -75,12 +100,12 @@ Solver::Solution& Solver::solve() {
 			}
 		}
 	}
-	if (!wasChanged) solution.profitNumb=0;
-	else {
-		for(Edge e:edges){
-			if (d[e.v]>d[e.u]+e.weight){
-				solution.profitNumb++;
-				solution.nodes.push_back(e.v);
+	if (wasChanged) {
+		for (Edge e:edges) {
+			if (d[e.v]>d[e.u]+e.weight && !visited[e.v]) {
+				visited[e.v]=true;
+				solution+e.v;
+				addNodes(e.v);
 			}
 		}
 	}
